d28c01413b4234a09331bb02dfc38e778095059e
Francisco Moreira <up201607929@fe.up.pt>
2018-12-04 11:57:27 +0000

    Update parser to proper format

--- Project2/src/client.c
+++ Project2/src/client.c
@@ -13,51 +13,96 @@
 #define STRING_LENGTH 50
 #define SOCKET_SIZE 1000
 
+struct Arguments {
+   char  user[50];
+   char  password[50];
+   char  host[50];
+   char  path[50];
+} arguments;
+
+int parseArguments(int argc, char **argv){
+
+  //Wrong argument size
+  if(argc < 2)
+    return 1;
+
+  //Raw string
+  char* raw = argv[1];
+
+  //Process string
+  char c;
+  //Global index
+  int i = 7;
+  //Local index
+  int li = 0;
+  int state = 0;
+  do{
+    c = raw[i];
+    switch(state){
+      case 0:
+        if(c == ':'){
+          arguments.user[li] = '\0';
+          state++;
+          li = 0;
+          break;
+        }
+        arguments.user[li] = c;
+        li++;
+        break;
+      case 1:
+        if(c == '@'){
+          arguments.password[li] = '\0';
+          state++;
+          li = 0;
+          //Because of ']'
+          i++;
+          break;
+        }
+        arguments.password[li] = c;
+        li++;
+        break;
+      case 2:
+        if(c == '/'){
+          arguments.host[li] = '\0';
+          state++;
+          li = 0;
+          break;
+        }
+        arguments.host[li] = c;
+        li++;
+        break;
+      case 3:
+        if(c == '\0'){
+          arguments.path[li] = '\0';
+          break;
+        }
+        arguments.path[li] = c;
+        li++;
+        break;
+      default:
+        break;
+    }
+    i++;
+  }while(c != '\0');
+
+  return 0;
+
+}
+
 int main(int argc, char **argv){
+
+  parseArguments(argc, argv);
+
+  printf("%s\n",arguments.user);
+  printf("%s\n",arguments.password);
+  printf("%s\n",arguments.host);
+  printf("%s\n",arguments.path);
 
   int socketfd;
 	int socketfdClient = -1;
 	struct sockaddr_in server_addr;
   struct sockaddr_in server_addr_client;
   char responseCode[3];
-
-  //Info strings
-  char user[STRING_LENGTH];
-  char pw[STRING_LENGTH];
-  char host[STRING_LENGTH];
-  char path[STRING_LENGTH];
-  char file[STRING_LENGTH];
-
-  //Reset strings
-  memset(user, 0, STRING_LENGTH);
-  memset(pw,0,STRING_LENGTH);
-  memset(host,0,STRING_LENGTH);
-  memset(path,0,STRING_LENGTH);
-  memset(file,0,STRING_LENGTH);
-
-  //Parse arguments
-  strcat(user, argv[1]);
-  strcat(pw, argv[2]);
-  strcat(host, argv[3]);
-  strcat(path, argv[4]);
-
-  //Parse file
-  int indexPath = 0;
-  int indexFile = 0;
-  char c;
-  do {
-    c = path[indexPath];
-    if(c == '/'){
-      indexFile = 0;
-    }else{
-      file[indexFile] = c;
-      indexFile++;
-    }
-    indexPath++;
-  } while(c != '\0');
-  file[indexFile++] = '\0';
-
-  printf("\n%s\n",file);
 
   return 0;
 }
