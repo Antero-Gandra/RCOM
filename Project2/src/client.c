ce6f3f12943e4d3735a625bf2e8cfdf7ad00a1b7
Dozed12 <up201607929@fe.up.pt>
2018-12-11 15:26:02 +0000

    Connection estabilished

--- Project2/src/client.c
+++ Project2/src/client.c
@@ -36,7 +36,7 @@ int parseArguments(int argc, char **argv)
   //Process string
   char c;
   //Global index
-  int i = 7;
+  int i = 6;
   //Local index
   int li = 0;
   int state = 0;
@@ -62,8 +62,6 @@ int parseArguments(int argc, char **argv)
         arguments.password[li] = '\0';
         state++;
         li = 0;
-        //Because of ']'
-        i++;
         break;
       }
       arguments.password[li] = c;
@@ -103,21 +101,104 @@ int parseArguments(int argc, char **argv)
 }
 
 //Get ip address according to the host's name
-struct hostent *getip(char* host)
+struct hostent *getip(char *host)
 {
-	struct hostent *h;
+  struct hostent *h;
 
-	if ((h = gethostbyname(host)) == NULL)
-	{
-		herror("gethostbyname");
-		exit(1);
-	}
+  if ((h = gethostbyname(host)) == NULL)
+  {
+    herror("gethostbyname");
+    exit(1);
+  }
 
-	return h;
+  return h;
+}
+
+//Get response code
+int response(int socket, char *code)
+{
+  int state = 0;
+  int i = 0;
+  char c;
+
+  printf("Response: ");
+
+  while (state != 3)
+  {
+    if (read(socket, &c, 1) == -1)
+      return -1;
+
+    printf("%c", c);
+
+    switch (state)
+    {
+    //3 digit number
+    case 0:
+      if (c == ' ')
+      {
+        if (i != 3)
+        {
+          printf(" > Error receiving response code\n");
+          return -1;
+        }
+        i = 0;
+        state = 1;
+      }
+      else
+      {
+        if (c == '-')
+        {
+          state = 2;
+          i = 0;
+        }
+        else
+        {
+          if (isdigit(c))
+          {
+            code[i] = c;
+            i++;
+          }
+        }
+      }
+      break;
+    //Read until end of line
+    case 1:
+      if (c == '\n')
+      {
+        state = 3;
+      }
+      break;
+    //Multi line code
+    case 2:
+      if (c == code[i])
+      {
+        i++;
+      }
+      else
+      {
+        if (i == 3 && c == ' ')
+        {
+          state = 1;
+        }
+        else
+        {
+          if (i == 3 && c == '-')
+          {
+            i = 0;
+          }
+        }
+      }
+      break;
+    }
+  }
+
+  return 0;
 }
 
 //  Example
-//  ./download ftp://[utilizador:pass@]fe.up.pt/pasta1/pasta2/imagem.png
+//  ./download ftp://utilizador:pass@ftp.fe.up.pt/pasta1/pasta2/imagem.png
+
+//TODO separate to functions most stuff
 int main(int argc, char **argv)
 {
 
@@ -139,32 +220,44 @@ int main(int argc, char **argv)
   struct hostent *h = getip(arguments.host);
 
   //Print IP
-  printf("IP Address: %s\n", inet_ntoa(*((struct in_addr *)h->h_addr)));
+  printf("\nIP Address: %s\n", inet_ntoa(*((struct in_addr *)h->h_addr)));
 
-  //Handle the address
+  //Handle address
   struct sockaddr_in server_addr;
-	bzero((char *)&server_addr, sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr)));
-	server_addr.sin_port = htons(SERVER_PORT);	
+  bzero((char *)&server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr)));
+  server_addr.sin_port = htons(SERVER_PORT);
 
   //Open TCP socket
   int socketfd;
-	if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		perror("socket()");
-		exit(0);
-	}
+  if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    perror("socket()");
+    exit(0);
+  }
 
   //Connect to Server
-	if (connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-	{
-		perror("connect()");
-		exit(0);
-	}
+  if (connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+  {
+    perror("connect()");
+    exit(0);
+  }
 
-  //TODO Read response
-  //TODO Confirm response
+  //Read response
+  char code[3];
+  if (response(socketfd, code) < 0)
+  {
+    perror("response()");
+    exit(0);
+  }
+
+  //Confirm response
+  if (code[0] == '2')
+  {
+    printf("Connection Estabilished\n");
+  }
+
   //TODO Send username
   //TODO Send password
   //TODO Get server port to be used
@@ -174,5 +267,4 @@ int main(int argc, char **argv)
   //TODO Close connection
 
   return 0;
-  
 }
